5bed4a5d93a8ab2bccd068bac9bb0ca74f97fb14
Sivaji gadidala <sivajigsivajig703@gmail.com>
2024-09-01 11:34:47 +0530

    operators added

---
+++ Operators/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators</title>
+</head>
+<body>
+    <h1>Operators</h1>
+    <?php
+    $a = "Hello";
+    $b = "World";
+    $num1 = 2;
+    $num2 = 4;
+    echo $a . " " . $b; //Concatenation
+    echo '<br>';
+    echo "Addition of num1 and num2 is ".$num1 + $num2;
+    echo '<br>';
+    echo "multiplication of num1 and num2 is ".$num1 * $num2;
+    ?>
+</body>
+</html>
